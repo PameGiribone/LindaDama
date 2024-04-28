81e5c00c4b500f0e29630b965881b904ebd8e6b4
juanscaffo <125586659+juanscaffo@users.noreply.github.com>
2024-04-27 21:12:03 -0300

    primer commit

---
+++ LindaDama/ProyectoLindaDama/index.js
@@ -0,0 +1,15 @@
+// SECTION TWO
+
+const btnGalery = document.querySelector(".btn-galery");
+const galeryImg = document.querySelector("galery-img")
+
+
+
+btnGalery.addEventListener('click', (e)=>{   
+        btnGalery.classList.remove("btn-galery");
+        btnGalery.classList.add("clase-nueva");
+
+    
+   
+   
+})
